b8289772d2d907b8e3076a420894c876ee60e862
Junaid Nazir <junaid.nazir@arbisoft.com>
2020-04-07 14:18:01 +0500

    Created a basic reddit article scrapper app.

---
+++ js/models/article.js
@@ -0,0 +1,15 @@
+// import Backbone from 'backbone';
+//
+// let Article = Backbone.Model.extend({
+//     url: 'https://www.reddit.com/r/worldnews/new.json',
+//     fetchData: async () => {
+//         const newsData = await this.fetch();
+//         console.log(newsData);
+//         // const newsArticles = newsData['data']['children'];
+//         // newsArticles.forEach((newsArticle, index) => {
+//         //     console.log(index);
+//         //     console.log(newsArticle.data)
+//         // });
+//     }
+// });
+// export default new Article();
---
+++ js/views/homepage.js
@@ -0,0 +1,20 @@
+// import article from "../models/article";
+//
+// let HomePage = Backbone.View.extend({
+//     initialize: function () {
+//         $('#get-started').html('Fetch Data');
+//     },
+//
+//     // events: {
+//     //     'click #get-started' : `fetchArticles`,
+//     // },
+//     //
+//     // fetchArticles: async function() {
+//     //     console.log('fetch data called');
+//     //     console.log(await this.model.fetchData());
+//     // }
+// });
+// let homePage = new HomePage({
+//     el: 'body',
+//     model: article
+// });
